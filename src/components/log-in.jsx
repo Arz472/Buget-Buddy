7abe888e8ac25159820678dd75c772e95dc2bdf1
Arz472 <s.ahmadreza.azizi@gmail.com>
2024-01-25 12:08:05 -0500

    chnage

--- src/components/log-in.jsx
+++ src/components/log-in.jsx
@@ -1,50 +1,53 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; 
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../components/log-in.css';
+import validation from '../components/validation';
 
-const LoginPage = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+function LoginPage() {
+    const [values, setValues] = useState({ email: '', password: '' });
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        console.log(`Email: ${email}`);
-        console.log(`Password: ${password}`);
-    }
+    const handleInput = (event) => {
+        setValues({ ...values, [event.target.name]: event.target.value });
+    };
+
+    const handleSubmit = (event) => {
+        
+    };
 
     return (
         <div className="container">
             <div className="form-container">
+                <h2>Log-In</h2>
                 <form onSubmit={handleSubmit}>
                     <div className="email-container">
                         <label htmlFor="email">Email</label>
                         <input
                             type="email"
+                            name="email"
                             placeholder="Enter Email"
-                            value={email}
-                            onChange={(event) => setEmail(event.target.value)}
+                            onChange={handleInput}
                         />
                     </div>
                     <div className="password-container">
                         <label htmlFor="password">Password</label>
                         <input
                             type="password"
+                            name="password"
                             placeholder="Enter Password"
-                            value={password}
-                            onChange={(event) => setPassword(event.target.value)}
+                            onChange={handleInput}
                         />
                     </div>
                     <button type="submit">Log In</button>
                     <div>
                         <p>Don't have an account?</p>
                     </div>
-                    <Link to="/sign-up"> {}
-                        <button type="button">Sign up</button> {}
+                    <Link to="/sign-up">
+                        <button type="button">Sign up</button>
                     </Link>
                 </form>
             </div>
         </div>
     );
-};
+}
 
 export default LoginPage;
--- src/components/sign-up.jsx
+++ src/components/sign-up.jsx
@@ -2,31 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
-    
-
     return (
         <div className="container">
             <div className="form-container">
+                <h2>Sign-Up</h2>
                 <form>
+                    <div className="name-container">
+                        <label htmlFor="name">Name</label>
+                        <input type="name" placeholder="Enter Name" />
+                    </div>
                     <div className="email-container">
                         <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            placeholder="Enter Email"
-                        />
+                        <input type="email" placeholder="Enter Email" />
                     </div>
                     <div className="password-container">
                         <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            placeholder="Enter Password"
-                        />
+                        <input type="password" placeholder="Enter Password" />
                     </div>
                     <button type="submit">Sign Up</button>
                     <div>
                         <p>Already have an account?</p>
                     </div>
-                    <Link to="/log-in">
+                    <Link to="/">
                         <button type="button">Log In</button>
                     </Link>
                 </form>
