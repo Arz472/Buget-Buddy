0b776614c19ec6f984c3fedb0ca51f5e2265b971
Arz472 <s.ahmadreza.azizi@gmail.com>
2024-01-25 09:45:16 -0500

    changes

--- src/components/log-in.jsx
+++ src/components/log-in.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../components/log-in.css';
 
 const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(`Email: ${email}`);
+        console.log(`Password: ${password}`);
+    }
+
     return (
-        <div>
-            <h1>Login Page</h1>
-            {}
+        <div className="container">
+            <div className="form-container">
+                <form onSubmit={handleSubmit}>
+                    <div className="email-container">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            placeholder="Enter Email"
+                            onChange={(event) => setEmail(event.target.value)}
+                        />
+                    </div>
+                    <div className="password-container">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            placeholder="Enter Password"
+                            onChange={(event) => setPassword(event.target.value)}
+                        />
+                    </div>
+                    <button type="submit">Log In</button>
+                </form>
+            </div>
         </div>
     );
 };
