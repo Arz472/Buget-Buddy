7f4f62c99d9dfd13cb2684c2f7c8decccdba6fda
Arz472 <s.ahmadreza.azizi@gmail.com>
2024-01-25 11:25:36 -0500

    more chnages

--- src/components/sign-up.jsx
+++ src/components/sign-up.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Signup() {
+const Signup = () => {
+    
+
     return (
-        <div>sign-up</div>
+        <div className="container">
+            <div className="form-container">
+                <form>
+                    <div className="email-container">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            placeholder="Enter Email"
+                        />
+                    </div>
+                    <div className="password-container">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            placeholder="Enter Password"
+                        />
+                    </div>
+                    <button type="submit">Sign Up</button>
+                    <div>
+                        <p>Already have an account?</p>
+                    </div>
+                    <Link to="/log-in">
+                        <button type="button">Log In</button>
+                    </Link>
+                </form>
+            </div>
+        </div>
     );
 }
 
